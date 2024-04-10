1d56cd524fb22995409a04423abca9272cc4a0e9
Jhonathan Vargas <sneyr845@gmail.com>
2024-04-09 19:28:07 -0500

    se rerigistra el controlador y la interfaz para el servicio de roles

---
+++ SmartFishLogin.Core/Interfaces/IRole.cs
@@ -0,0 +1,14 @@
+﻿using SmartFishLogin.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFishLogin.Core.Interfaces
+{
+    public interface IRole
+    {
+        Task<LoginResultDto> Register(LoginRequestDto param);
+    }
+}
---
+++ SmartFishLogin/Controllers/RoleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartFishLogin.Core.Dtos;
+using SmartFishLogin.Core.Interfaces;
+using SmartFishLogin.Helpers;
+
+namespace SmartFishLogin.Controllers
+{
+    [Route("[controller]/[action]")]
+
+    public class RoleController : Controller
+    {
+        private readonly IRole _role;
+
+        public RoleController(IRole role)
+        {
+            _role = role;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto param)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(new ResponseErrorApi(ModelState));
+                var result = await _role.Register(param);
+                return Ok(ResponseApi.Response(false, result, null, null));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"log error: {ex.Message}");
+                return BadRequest(ResponseApi.Response(true, null, "No se proceso la información", null));
+            }
+
+        }
+    }
+}
